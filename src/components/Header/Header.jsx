0aaa4b4c076054ffe70cebc9911344a53cf23270
Maya Kazakova <anais.maya@gmail.com>
2025-01-15 00:41:58 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
+import { StyledHeader, Logo, NavList, StyledNavLink } from './Header.styled';
+import icons from '../../assets/icons/sprite.svg';
+
 const Header = () => {
-    return (
-        <div>Header</div>
-    )
+  return (
+    
+    <StyledHeader>
+       
+      <Logo>
+      <use href={`${icons}#icon-Logo`} />
+      </Logo>
+      <NavList>
+          <li>
+            <StyledNavLink to="/" end>
+              Home
+            </StyledNavLink>
+          </li>
+          <li>
+            <StyledNavLink to="/catalog">Catalog</StyledNavLink>
+          </li>
+          {/* <li>
+            <StyledNavLink to="/favorites">Favorites</StyledNavLink>
+          </li> */}
+        </NavList>
+    </StyledHeader>
+    
+  );
 };
 
 export default Header;
+
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,51 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+position: sticky;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color:  var(--inputs);
+  /* position: relative; */
+  width: 100%;
+  height: 72px;
+  border-bottom: 1px solid var(--badges);
+  
+`;
+
+export const Logo = styled.svg`
+position: absolute;
+top: 28px;
+left: 64px;
+width: 136px;
+height: 15px;
+  
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 20px;
+  justify-content: center; 
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: var(--main);
+  font-size: 16px;
+
+  &.active {
+    color: var(--button-hover);
+    font-weight: 500;
+    padding-bottom: 2px;
+  }
+
+  &:hover {
+    color: var(--gray-light);
+    transition: var(--transition);
+  }
+`;
