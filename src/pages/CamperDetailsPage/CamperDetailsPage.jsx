4ef32adf370b1e47cfe7f0a666e7f948474fc2bc
Maya Kazakova <anais.maya@gmail.com>
2025-01-16 17:24:35 +0100

    GoBackButton

--- src/pages/CamperDetailsPage/CamperDetailsPage.jsx
+++ src/pages/CamperDetailsPage/CamperDetailsPage.jsx
@@ -1,5 +1,10 @@
+import { LinkBack } from "./CamperDetailsPage.styled"
+
 export default function CamperDetailsPage() {
     return (
+        <>
+        <LinkBack to={'/catalog'}><span>&#8592;</span> Back</LinkBack>
         <div>CamperDetailsPage</div>
+        </>
     )
 }
--- src/pages/CamperDetailsPage/CamperDetailsPage.styled.js
+++ src/pages/CamperDetailsPage/CamperDetailsPage.styled.js
@@ -1,5 +1,27 @@
-export default function CamperDetailsPage() {
-    return (
-        <div>CamperDetailsPage</div>
-    )
-}
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const LinkBack = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  padding: 8px;
+  margin-bottom: 24px;
+  width: 120px;
+  border: 1px solid var(--gray-light);
+  border-radius: 200px;
+
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 150%;
+  letter-spacing: -0.01em;
+
+  transition: border-color var(--transition);
+  cursor: pointer;
+  &:hover,
+   :focus {
+    border: 1px solid var(--button-hover);
+  }
+`
